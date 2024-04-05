ae534535e6ce4d2f9bd8d7597640c60bf82fad10
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-04-05 13:41:59 +0530

    initiate sample project in spring boot

---
+++ hello-springboot/src/main/java/lk/ijse/gdse66/hellospringboot/dto/CustomerDTO.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse66.hellospringboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO implements Serializable {
+    private String id;
+    private String name;
+    private String address;
+}
---
+++ hello-springboot/src/main/java/lk/ijse/gdse66/hellospringboot/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,53 @@
+package lk.ijse.gdse66.hellospringboot.service.impl;
+
+import lk.ijse.gdse66.hellospringboot.dto.CustomerDTO;
+import lk.ijse.gdse66.hellospringboot.entity.CustomerEntity;
+import lk.ijse.gdse66.hellospringboot.repository.CustomerRepo;
+import lk.ijse.gdse66.hellospringboot.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
+public class CustomerServiceImpl implements CustomerService {
+    CustomerRepo customerRepo;
+    ModelMapper mapper;
+
+    public CustomerServiceImpl(CustomerRepo customerRepo, ModelMapper mapper) {
+        this.customerRepo = customerRepo;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return customerRepo.findAll().stream().map(
+                customer -> mapper.map(customer, CustomerDTO.class)).toList();
+    }
+
+    @Override
+    public CustomerDTO getCustomerDetails(String id) {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
+        customerDTO.setId(UUID.randomUUID().toString());
+        return mapper.map(customerRepo.save(mapper.map(
+                customerDTO, CustomerEntity.class)), CustomerDTO.class);
+    }
+
+    @Override
+    public void updateCustomer(String id, CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+}
