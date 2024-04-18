71dfba77d0901ff2de12ab37715c4490461f9aaa
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-04-18 11:28:05 +0530

    implement basic authentication

---
+++ spring-boot-with-basic-auth/src/main/java/lk/ijse/gdse66/springbootwithbasicauth/repository/CustomerRepo.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse66.springbootwithbasicauth.repository;
+
+
+import lk.ijse.gdse66.springbootwithbasicauth.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<CustomerEntity,String> {
+}
